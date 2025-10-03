ccffbddaf62e66bd611798dab804eac0cae87d96
Lucas-Rojas-Nahuel <rolukasro12@gmail.com>
2025-10-03 12:07:04 -0300

    refactor: refactoring Gameboard and its methods

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -5,20 +5,20 @@ export class Gameboard {
     this.row = row ? row : 10;
     this.colum = colum ? colum : 10;
     this.board = initializeBoard(this.row, this.colum);
-    this.misses = [];
+    this.misses = new Set();
+    this.ships = [];
+    this.attackedCoords = new Set();
   }
 
   placeShip(ship, coordinate, orientation) {
     const [x, y] = coordinate;
+    const positions = [];
 
-    if (x < 0 || x >= this.row || y < 0 || y >= this.colum) {
+    if (x < 0 || x >= this.row || y < 0 || y >= this.colum)
       throw new Error("Invalid placement");
-    }
 
     if (orientation === "horizontal") {
-      if (x + ship.length > this.row) {
-        throw new Error("Invalid placement");
-      }
+      if (x + ship.length > this.row) throw new Error("Invalid placement");
 
       for (let i = 0; i < ship.length; i++) {
         const cellkey = `${x + i},${y}`;
@@ -26,16 +26,10 @@ export class Gameboard {
         if (this.board[cellkey] !== "empty") {
           throw new Error("Overlap not allowed");
         }
-      }
-
-      for (let i = 0; i < ship.length; i++) {
-        const cellkey = `${x + i},${y}`;
-        this.board[cellkey] = ship;
+        positions.push([x + i, y]);
       }
     } else if (orientation === "vertical") {
-      if (y + ship.length > this.colum) {
-        throw new Error("Invalid placement");
-      }
+      if (y + ship.length > this.colum) throw new Error("Invalid placement");
 
       for (let i = 0; i < ship.length; i++) {
         const cellkey = `${x},${y + i}`;
@@ -43,13 +37,18 @@ export class Gameboard {
         if (this.board[cellkey] !== "empty") {
           throw new Error("Overlap not allowed");
         }
-      }
-
-      for (let i = 0; i < ship.length; i++) {
-        const cellkey = `${x},${y + i}`;
-        this.board[cellkey] = ship;
+        positions.push([x, y + i]);
       }
     }
+
+    positions.forEach(([px, py]) => {
+      this.board[`${px},${py}`] = ship;
+    });
+
+    this.ships.push({
+      ship,
+      positions,
+    });
   }
 
   receiveAttack(coordinate) {
@@ -60,53 +59,27 @@ export class Gameboard {
       throw new Error("Invalid placement");
     }
 
-    //verificamos de que no repita la coordenada
-    const isRepeated = this.misses.some(
-      (arr) =>
-        arr.length === coordinate.length &&
-        arr.every((element, index) => element === coordinate[index])
-    );
+    const cellkey = `${x},${y}`;
 
-    if (isRepeated) {
+    //verificar repetidos en misses + hits
+    if (this.misses.has(cellkey) || this.attackedCoords.has(cellkey)) {
       throw new Error("Already attacked");
     }
 
-    const cellkey = `${x},${y}`;
-
     //si encuentra un barco actualiza su estado de golpe
     if (this.board[cellkey] !== "empty") {
-      this.board[cellkey].hit();
+      const ship = this.board[cellkey];
+      ship.hit();
+      this.attackedCoords.add(cellkey);
+      return "hit";
+    } else {
+      this.misses.add(cellkey);
+      return "miss";
     }
-
-    //guardamos las cordenadas para no repetir los ataques
-    this.misses.push(coordinate);
   }
 
   allShipsSunk() {
-    //guardamos el estado de todos los barcos que se encuentra
-    const ships = [];
-    for (let x = 0; x < this.row; x++) {
-      for (let y = 0; y < this.colum; y++) {
-        let cellkey = `${x},${y}`;
-        if (this.board[cellkey] !== "empty") {
-          ships.push(this.board[cellkey].isSunk());
-        }
-      }
-    }
-
     //si se undieron todos los barcos devuelve true, si no devuelve false
-    if (ships.length === 1) {
-      return ships[0];
-    } else {
-      let isTrue = false;
-      for (let i = 0; i < ships.length; i++) {
-        if (ships[i] === true) {
-          isTrue = true;
-        } else {
-          return false;
-        }
-      }
-      return isTrue;
-    }
+    return this.ships.every((ship) => ship.ship.isSunk());
   }
 }
